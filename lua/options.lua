6c8bd36d3413cec3e27ea0b0a550def5136955e0
EddieCheng <chengxzhcuhk@gmail.com>
2024-03-17 21:40:25 +0800

    updating default colorscheme

--- lua/options.lua
+++ lua/options.lua
@@ -1,12 +1,14 @@
 vim.cmd([[ autocmd vimenter * hi Comment cterm = italic gui = italic ]])
 vim.cmd([[ autocmd vimenter * hi Statement cterm = italic gui = italic ]])
+-- vim.cmd([[ autocmd vimenter * hi LineNr guibg=orange ]])
 vim.cmd([[ autocmd vimenter * TSEnable highlight ]])
 vim.cmd([[ autocmd vimenter * hi Normal ctermbg=none guibg=none ]])
 vim.cmd([[ set rtp+=/usr/local/lib/python2.7/site-packages/powerline/bindings/vim/ ]])
 vim.cmd([[ let t_Co=256 ]])
 vim.cmd([[let &t_ZH="\e[3m"]])
 vim.cmd([[let &t_ZR="\e[23m"]])
-vim.cmd([[ colorscheme gruvbox-baby ]])
+-- vim.cmd([[ colorscheme gruvbox-baby ]])
+vim.cmd([[ colorscheme gruvbox ]])
 vim.cmd([[ set encoding=UTF-8 ]])
 vim.cmd([[ set conceallevel=1 ]])
 vim.g.indentLine_conceallevel=1
@@ -18,7 +20,7 @@ vim.cmd([[ noremap <space>f "lua vim.lsp.buf.format()" ]])
 vim.opt.mouse=a
 vim.g.mapleader=","
 vim.opt.cursorline=true
-vim.opt.cursorcolumn=true
+-- vim.opt.cursorcolumn=true
 vim.opt.relativenumber=true
 vim.opt.number=true
 vim.opt.termguicolors=true
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,29 +1,5 @@
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['clangd'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities
-}
-
-require('lspconfig')['pyright'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities
-}
-
--- require('lspconfig')['sumneko_lua'].setup {
---     on_attach = on_attach,
---     flags = lsp_flags,
---     capabilities = capabilities
--- }
-require('lspconfig')['lua_ls'].setup { -- this is to fix the deprecated warning of sumneko_lua
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities
-}
-
 -- key bindings
 
 -- Mappings.
@@ -65,3 +41,28 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
+
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['clangd'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities
+}
+
+require('lspconfig')['pyright'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities
+}
+
+-- require('lspconfig')['sumneko_lua'].setup {
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+--     capabilities = capabilities
+-- }
+require('lspconfig')['lua_ls'].setup { -- this is to fix the deprecated warning of sumneko_lua
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities
+}
+
